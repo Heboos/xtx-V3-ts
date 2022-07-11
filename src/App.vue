b0ba2ef9c2c356f992190bf805f156137423d5c4
kangkang <2729907104@qq.com>
2022-07-11 12:01:41 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div>
+  <h1>小兔仙</h1>
+</div>
+</template>
+<style>
+
+</style>
---
+++ src/utils/request.ts
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+// 备用接口地址: http://pcapi-xiaotuxian-front-devtest.itheima.net/
+const instance = axios.create({
+  // 请求基地址
+  baseURL: 'http://pcapi-xiaotuxian-front.itheima.net/',
+  // 最长等待时间，如果超过了这个时间，就报错
+  timeout: 5000
+})
+
+// 添加请求拦截器
+instance.interceptors.request.use(
+  function (config) {
+    // 在发送请求之前做些什么
+    return config
+  },
+  function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error)
+  }
+)
+
+// 添加响应拦截器
+instance.interceptors.response.use(
+  function (response) {
+    return response
+  },
+  function (error) {
+    // 对响应错误做点什么
+    return Promise.reject(error)
+  }
+)
+
+export default instance
