6952d849677c5fc97a6889022aac80fe638e4a4a
kangkang <2729907104@qq.com>
2022-07-12 15:16:43 +0800

    骨架图组件的封装完成

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import vue from '@vitejs/plugin-vue'
 const path = require('path')
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue({
+    reactivityTransform: true
+  })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
