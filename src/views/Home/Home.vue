82b0174ae64e0509034d9a2e622a54535c9267bb
kangkang <2729907104@qq.com>
2022-07-14 10:10:53 +0800

    新鲜好物人气推荐类型公共组件封装并测试

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import HomeBanner from './components/HomeBanner.vue'
 import HomeCategory from './components/HomeCategory.vue'
+import HomePanel from './components/HomePanel.vue';
 </script>
 
 <template>
@@ -12,6 +13,14 @@ import HomeCategory from './components/HomeCategory.vue'
         <!-- banner轮播图 -->
         <HomeBanner />
       </div>
+      <HomePanel title="新鲜好物" sub-title="新鲜出炉 品质靠谱">
+        <template #right>
+          <XtxMore path="/"></XtxMore>
+        </template>
+      </HomePanel>
+      <HomePanel title="人气推荐" sub-title="人气爆款 不容错过">
+        1111
+      </HomePanel>
     </div>
   </div>
 </template>
