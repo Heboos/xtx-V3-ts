00423bda91cc08a99062720480d63cbfa57d055d
kangkang <2729907104@qq.com>
2022-07-15 10:20:58 +0800

    完成面包屑组件封装

--- src/components/XtxBread/XtxBread.vue
+++ src/components/XtxBread/XtxBread.vue
@@ -6,10 +6,29 @@ provide('separtor', separtor)
 </script>
 
 <template>
-    <slot></slot>
-
+  <div class="xtx-bread">
+    <slot />
+  </div>
 </template>
 
 <style lang='less' scoped>
-
+.xtx-bread {
+  display: flex;
+  padding: 25px 10px;
+  &-item {
+    a {
+      color: #666;
+      transition: all 0.4s;
+      &:hover {
+        color: @xtxColor;
+      }
+    }
+  }
+  i {
+    font-size: 12px;
+    margin-left: 5px;
+    margin-right: 5px;
+    line-height: 22px;
+  }
+}
 </style>
--- src/components/XtxBread/XtxBreadItem.vue
+++ src/components/XtxBread/XtxBreadItem.vue
@@ -6,12 +6,32 @@ import { inject } from 'vue';
 </script>
 
 <template>
-
-  <slot></slot>
-  <!-- 分隔符 -->
-  {{ separtor }}
+  <div class="xtx-bread-item">
+    <!--
+      如果to存在 有值 我们就渲染一个router-link标签
+      如果to不存在  那就渲染一个span标签
+    -->
+    <!-- 是否需要跳转 -->
+    <router-link v-if="to" :to="to"><slot /></router-link>
+    <span v-else><slot /></span>
+    
+    <!-- 分隔符 -->
+    <i v-if="separtor">{{ separtor }}</i>
+    <i v-else class="iconfont icon-angle-right"></i>
+  </div>
 </template>
 
 <style lang='less' scoped>
-
+.xtx-bread-item {
+  i {
+    margin: 0 6px;
+    font-size: 10px;
+  }
+  // 最后一个i隐藏
+  &:nth-last-of-type(1) {
+    i {
+      display: none;
+    }
+  }
+}
 </style>
